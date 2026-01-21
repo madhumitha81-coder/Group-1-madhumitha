a82fd66ce741f2a9bc21f68e779050b44db8c7b4
Madhumitha <padmakorota@gmail.com>
2026-01-21 16:49:31 +0530

    Add wsgi.py for deployment

--- Python-TalentLink/TalentLink1/TalentLink/wsgi.py
+++ Python-TalentLink/TalentLink1/TalentLink/wsgi.py
@@ -1,24 +1,9 @@
-"""
-WSGI config for TalentLink1 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-import sys
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# ADD PROJECT ROOT TO PYTHON PATH
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
-
 os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'TalentLink.settings'
+    "DJANGO_SETTINGS_MODULE",
+    "TalentLink.settings"
 )
 
 application = get_wsgi_application()

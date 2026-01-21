4a98fae9b0157beec64ebd7fd40601faf1b69035
Madhumitha <padmakorota@gmail.com>
2026-01-21 12:10:54 +0530

    Add all project files for Render deployment

--- Python-TalentLink/TalentLink1/TalentLink/wsgi.py
+++ Python-TalentLink/TalentLink1/TalentLink/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+# ADD PROJECT ROOT TO PYTHON PATH
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
